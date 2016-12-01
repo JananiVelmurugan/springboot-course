2272a2b5d9720d804626cca48cdce5321d160b96
Janani <jananise@gmail.com>
2016-12-01 11:15:09 +0530

    Spring4 main class

--- Spring4/src/main/java/com/janani/spring/Spring4Application.java
+++ Spring4/src/main/java/com/janani/spring/Spring4Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring4Application.class, args);
+		SpringApplication.run(Spring4Application.class, args); 
 	}
 }
